a118be96bc942323c289d545bd9e4e44332a1d82
NG WEI HEN <rolandng0001@gmail.com>
2023-05-30 23:00:37 +0800

    Lab 11

--- restorderapp/src/main/java/my/edu/utem/ftmk/dad/restorderapp/repository/OrderTypeRepository.java
+++ restorderapp/src/main/java/my/edu/utem/ftmk/dad/restorderapp/repository/OrderTypeRepository.java
@@ -1,6 +1,9 @@
 package my.edu.utem.ftmk.dad.restorderapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import my.edu.utem.ftmk.dad.restorderapp.model.OrderType;
@@ -8,4 +11,36 @@ import my.edu.utem.ftmk.dad.restorderapp.model.OrderType;
 @Repository
 public interface OrderTypeRepository extends JpaRepository<OrderType, Long> {
 
+	/*
+	 * Lab 11 - Task 2.1 for Find Ordertype by Name
+	 * This define a new abstract method named findByNameContaining()
+	 * 
+	 * @Param partCode
+	 * @Return A list of order type data with successfully
+	 */
+	public abstract List<OrderType> findByNameContaining (String partName);
+	
+	/*
+	 * Lab 11 - Task 3.1
+	 * This define a new abstract method named findByCodeStartingWith()
+	 * 
+	 * @Param partCode
+	 * @Return A list of order type data
+	 */
+	public abstract List<OrderType> findByCodeStartingWith(String partCode);
+	
+	/*
+	 * Lab 11 - Task 5.1
+	 * This define a new abstract method named findByOrderByNameAsc()
+	 * @Return A list of order type data
+	 */
+	public abstract List<OrderType> findByOrderByNameAsc();
+	
+	/*
+	 * From Lecture Week 11
+	 */
+	// Custom query
+	@Query(value = "select * from OrderType where code like '%PU%'",
+			nativeQuery = true)
+	public List<OrderType> findByCode();
 }
